1eda16cfccf9e62ced505e812ec9eee67adc3490
Dragan Shutinvsoki <dragan.shutinovski@endava.com>
2015-10-14 23:09:19 +0200

    Clean Code

--- MvcRefactorTest/Infrastructure/Abstract/ICustomMembershipProvider.cs
+++ MvcRefactorTest/Infrastructure/Abstract/ICustomMembershipProvider.cs
@@ -5,17 +5,29 @@ namespace MvcRefactorTest.Infrastructure.Abstract
     public interface ICustomMembershipProvider
     {
         string ApplicationName { get; set; }
+
         bool EnablePasswordReset { get; }
+        
         bool EnablePasswordRetrieval { get; }
+        
         int MaxInvalidPasswordAttempts { get; }
+        
         int MinRequiredNonAlphanumericCharacters { get; }
+        
         int MinRequiredPasswordLength { get; }
+        
         int PasswordAttemptWindow { get; }
+        
         MembershipPasswordFormat PasswordFormat { get; }
+        
         string PasswordStrengthRegularExpression { get; }
+        
         bool RequiresQuestionAndAnswer { get; }
+        
         bool RequiresUniqueEmail { get; }
+        
         bool ChangePassword(string username, string oldPassword, string newPassword);
+        
         bool Authenticate(string username, string password);
 
         bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion,
@@ -25,20 +37,30 @@ namespace MvcRefactorTest.Infrastructure.Abstract
             string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status);
 
         bool DeleteUser(string username, bool deleteAllRelatedData);
+        
         MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords);
 
         MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize,
             out int totalRecords);
 
         MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords);
+        
         int GetNumberOfUsersOnline();
+        
         string GetPassword(string username, string answer);
+        
         MembershipUser GetUser(object providerUserKey, bool userIsOnline);
+        
         MembershipUser GetUser(string username, bool userIsOnline);
+        
         string GetUserNameByEmail(string email);
+        
         string ResetPassword(string username, string answer);
+        
         bool UnlockUser(string userName);
+        
         void UpdateUser(MembershipUser user);
+        
         bool ValidateUser(string username, string password);
     }
 }
